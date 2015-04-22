a4577517617b37b8496698959d648a427f3d36e7
PreyDelaCruz <amritaprey@gmail.com>
2015-04-22 18:42:57 +0800

    Improved UI... again.

--- app/view/homepage/Profile.js
+++ app/view/homepage/Profile.js
@@ -1,26 +1,24 @@
 Ext.define('Stadtra.view.homepage.Profile', {
     extend: 'Ext.panel.Panel',
+ 
     xtype: 'profile',
-    
-    
+        
     title: 'Profile',
 
     layout: 'center',
-    
-    defaults: {
-        layout: 'form',
-        xtype: 'container',
-        style: 'width: 50%',
-        width: 700
-    },
     
     autoScroll: true,
 
     items: [
 		{
+	        layout: 'form',
+	        xtype: 'container',
+	        width: 700,
+
 			items: [
 				{	
 					xtype: 'form',
+			        padding: 10,
 					defaults: {
 						xtype: 'displayfield',
 						labelWidth: 200
@@ -38,6 +36,83 @@ Ext.define('Stadtra.view.homepage.Profile', {
 						{ fieldLabel: 'List of Previous Advisers' },
 						{ fieldLabel: 'Current Adviser' },
 						{ xtype: 'button', text: 'Change Adviser' }
+					]
+				},
+				{	
+					xtype: 'grid',
+			        padding: 10,
+					title: 'Subjects Currently Taking',
+					maxHeight: 450,
+					width : 700,
+					columns: [
+						{
+							text: 'Number',
+							flex: 1,
+							dataIndex: 'courseNumber'
+						},
+						{
+							text: 'Title',
+							flex: 1,
+							dataIndex: 'courseTitle'
+						},
+						{
+							text: 'Section',
+							flex: 1,
+							dataIndex: 'section'
+						},
+						{
+							text: 'Units',
+							flex: 1,
+							dataIndex: 'unit'
+						},
+						{
+							text: 'Lecture',
+							flex: 1,
+							dataIndex: 'lecture'
+						},
+						{
+							text: 'Lecturer',
+							flex: 1,
+							dataIndex: 'lecturer'
+						},{
+							text: 'Lab/Recit',
+							flex: 1,
+							dataIndex: 'labRecit'
+						},
+						{
+							text: 'Instructor',
+							flex: 1,
+							dataIndex: 'instructor'
+						}
+					]
+				},
+				{	
+					xtype: 'grid',
+			        padding: 10,
+					title: 'Overall Grades',
+					maxHeight: 450,
+					width : 700,
+					columns: [
+						{
+							text: 'Course',
+							flex: 1,
+							dataIndex: 'course'
+						},
+						{
+							text: 'Sem/Year Taken',
+							flex: 1,
+							dataIndex: 'sem'
+						},
+						{
+							text: 'Units',
+							flex: 1,
+							dataIndex: 'units'
+						},
+						{
+							text: 'Grade',
+							flex: 1,
+							dataIndex: 'grade'
+						}
 					]
 				}
 			]
